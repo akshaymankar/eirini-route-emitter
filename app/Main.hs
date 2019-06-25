68f32681febde29039916389fe72b394405e0ecf
Akshay Mankar <itsakshaymankar@gmail.com>
2019-06-25 18:17:43 +0100

    Collect and publish routes every 30 seconds

--- app/Main.hs
+++ app/Main.hs
@@ -10,16 +10,23 @@ import Control.Concurrent.STM
 import Network.Nats.Client
 import FreerKube.Response
 import FreerNats.Client
+import Control.Concurrent
+import Control.Monad
 
 import qualified Data.Map as Map
 
 main :: IO ()
 main = do
-  let gkeConfig = "/Users/axeman/.kube/config"
+  let gkeConfig = "/Users/eirini/.kube/config"
       ibmConfig = "/Users/axeman/.bluemix/plugins/container-service/clusters/acceptance/kube-config-lon06-acceptance.yml"
   oidcCache <- atomically $ newTVar $ Map.fromList []
-  (mgr,cfg) <- kubeClient oidcCache $ KubeConfigFile ibmConfig
-  routes <- interpretKubeResponseInIO mgr cfg (collectRoutes $ Namespace "eirini")
+  (mgr,cfg) <- kubeClient oidcCache $ KubeConfigFile gkeConfig
   client <- connect defaultConnectionSettings 10
   subject <- either error pure $ createSubject "router.register"
+  forever $ collectAndPublish mgr cfg client subject >> (threadDelay $ 30 * seconds)
+
+collectAndPublish mgr cfg client subject = do
+  routes <- interpretKubeResponseInIO mgr cfg (collectRoutes $ Namespace "eirini")
   interpretNatsOperationInIO client $ publishRoutes subject routes
+
+seconds = 1000000
